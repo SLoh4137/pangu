cee571c51abc0c6e6d769303c098600085014ecb
Stephan Loh <sloh4137@gmail.com>
2020-04-15 22:24:01 -0400

    Create initial state and scriptable states

---
+++ Assets/Scripts/Player/States/CharacterState.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace pangu
+{
+    public class CharacterState : StateMachineBehaviour
+    {
+        public List<StateData> ListAbilityData = new List<StateData>();
+        private CharacterControl characterControl;
+        public CharacterControl GetCharacterControl(Animator animator)
+        {
+            if (characterControl == null)
+            {
+                characterControl = animator.GetComponentInParent<CharacterControl>();
+            }
+            return characterControl;
+        }
+
+        public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+            CharacterControl characterControl = GetCharacterControl(animator);
+            foreach(StateData state in ListAbilityData) {
+                state.OnEnter(characterControl, animator, stateInfo);
+            }
+        }
+
+        public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+            CharacterControl characterControl = GetCharacterControl(animator);
+            foreach(StateData state in ListAbilityData) {
+                state.Update(characterControl, animator, stateInfo);
+            }
+        }
+
+        public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+            CharacterControl characterControl = GetCharacterControl(animator);
+            foreach(StateData state in ListAbilityData) {
+                state.OnExit(characterControl, animator, stateInfo);
+            }
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/Player/States/StateData.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace pangu
+{
+    public abstract class StateData : ScriptableObject
+    {
+        public float Duration;
+    
+        public abstract void OnEnter(CharacterControl characterControl, Animator animator, AnimatorStateInfo stateInfo);
+        public abstract void Update(CharacterControl characterControl, Animator animator, AnimatorStateInfo stateInfo);
+        public abstract void OnExit(CharacterControl characterControl, Animator animator, AnimatorStateInfo stateInfo);
+    }
+}
+
