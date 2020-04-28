e6cf0b5afb9e340aa20c44c519751fcf34001dc7
Stephan Loh <sloh4137@gmail.com>
2020-04-28 15:34:11 -0400

    Pre-refactor of state

--- Assets/Scripts/States/BaseScripts/Jump.cs
+++ Assets/Scripts/States/BaseScripts/Jump.cs
@@ -4,25 +4,41 @@ using UnityEngine;
 
 namespace pangu.states
 {
+    
     [CreateAssetMenu(fileName = "New State", menuName = "Pangu/Ability/Jump")]
     public class Jump : StateData
     {
-        public float JumpForce = 10f;
-        private float precomputeJumpHeight;
+        public Vector2 JumpForceVector;
+        public float JumpTimeLength;
+        private float elapsedTime;
+        private float nextTime;
         
         public override void OnEnter(CharacterControl characterControl, Animator animator, AnimatorStateInfo stateInfo)
         {
             // Unused
         }
         public override void UpdateAbility(CharacterControl characterControl, Animator animator, AnimatorStateInfo stateInfo)
-        {
+        { 
+            // if(characterControl.Jump && characterControl.CurrentJump < characterControl.MaxJumps) {
+            //     elapsedTime = 0;
+            //     characterControl.CurrentJump++;
+            // }
+
+            // while(elapsedTime < JumpTimeLength) {
+            //     float proportionCompleted = elapsedTime / JumpTimeLength;
+            //     Vector2 curForceVector = Vector2.Lerp(JumpForceVector, Vector2.zero, proportionCompleted);
+            //     characterControl.rb.AddForce(curForceVector);
+            //     elapsedTime += Time.deltaTime;
+            // }
+
+
             if(!characterControl.Jump || characterControl.CurrentJump >= characterControl.MaxJumps) return; // Not jumping 
 
-            Debug.Log("called");
             characterControl.CurrentJump++;
-            characterControl.rb.velocity += Vector2.up * JumpForce;
+            // characterControl.rb.AddForce(JumpForceVector);
+            characterControl.rb.velocity = new Vector2(characterControl.rb.velocity.x, 0) + JumpForceVector;
 
-            //Vector3 velocity = characterControl.Movement;
+            // Vector3 velocity = characterControl.Movement;
             // characterControl.Movement += Vector3.up * precomputeJumpHeight;
             
         }
