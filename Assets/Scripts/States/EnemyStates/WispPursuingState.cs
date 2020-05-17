f32f3fbb4d1ada4da446d66a191c00bd3dbfffb4
Stephan Loh <sloh4137@gmail.com>
2020-05-17 00:01:26 -0400

    Made it so wisps won't stop pursuing once seen

--- Assets/Scripts/States/EnemyStates/WispPursuingState.cs
+++ Assets/Scripts/States/EnemyStates/WispPursuingState.cs
@@ -13,10 +13,10 @@ namespace pangu
         public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
         {
             Control.transform.position = PathfindingManager.Instance.MoveTowardsPlayer(Control.transform, Control.Stats.Speed.Value);
-            if (!PathfindingManager.Instance.WithinDistance(Control.transform, Control.Stats.SensingRadius.Value))
-            {
-                animator.SetBool(EnemyTransition.isPlayerSensed.ToString(), false);
-            }
+            // if (!PathfindingManager.Instance.WithinDistance(Control.transform, Control.Stats.SensingRadius.Value))
+            // {
+            //     animator.SetBool(EnemyTransition.isPlayerSensed.ToString(), false);
+            // }
         }
 
         public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
