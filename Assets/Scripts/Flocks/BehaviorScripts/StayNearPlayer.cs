04901b6921011201aa41a07c0f60c472af96979f
Stephan Loh <sloh4137@gmail.com>
2020-05-17 14:27:43 -0400

    Unclaimed flock now tries to stay near the player

---
+++ Assets/Scripts/Flocks/BehaviorScripts/StayNearPlayer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace pangu
+{
+    [CreateAssetMenu(menuName= "Flock/Behavior/Stay near Player")]
+    public class StayNearPlayerBehavior : FlockBehavior
+    {
+        private Transform player;
+        public float radius = 10f;
+        public float pullRadius = 0.9f;
+
+        private Transform Player { 
+            get {
+                if(player == null)
+                {
+                    player = Director.Instance.Player;
+                }
+                return player;
+            }
+        }
+        public override Vector2 CalculateMove(FlockAgent agent, List<Transform> context, Flock flock)
+        {
+            Vector2 centerOffset = (Vector2) (Player.transform.position - agent.transform.position);
+            float t = centerOffset.magnitude / radius;
+
+            // If the agent is within the pull radius, then don't need to influence
+            if (t < pullRadius) {
+                return Vector2.zero;
+            }
+
+            return centerOffset * t * t;
+        }
+
+    }
+}
